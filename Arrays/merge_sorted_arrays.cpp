2058024d6eaa8b65b2d464e0c89072adce3d6fcb
Ronan_1803 <ronan.fonseca1803@gmail.com>
2025-10-08 21:43:40 +0530

    Add solution for Merge Sorted Arrays problem

---
+++ Arrays/merge_sorted_arrays.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int p = m - 1;
+        int q = n - 1;
+        int i = m + n - 1;
+        while (p >= 0 && q >= 0) {
+            if (nums1[p] > nums2[q]) {
+                nums1[i] = nums1[p];
+                p--;
+            } else {
+                nums1[i] = nums2[q];
+                q--;
+            }
+            i--;
+        }
+        while (q >= 0) {
+            nums1[i] = nums2[q];
+            q--;
+            i--;
+        }
+    }
+};
